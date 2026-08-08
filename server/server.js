de715b1372c453ee111444ee6df9768b0a24acec
pseudosignal <chadcopywriter@gmail.com>
2026-08-08 22:24:55 +1000

    changed port back to 3000

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const express = require("express");
 const { runChecks } = require("./checks");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const CLIENT_DIR = path.join(__dirname, "..", "client");
 
 app.use(express.json({ limit: "1mb" }));
